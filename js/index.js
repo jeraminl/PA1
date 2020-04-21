561047ec7bdbd5e6bc876cba1beff20a1c361c44
jeraminl <jeraminl@uci.edu>
2020-04-20 21:51:20 -0700

    Merge branch 'master' of C:\Users\mattl\Documents\Projects\PA1 with conflicts.

--- js/index.js
+++ js/index.js
@@ -23,16 +23,36 @@ function load(){
         console.log(data.id[i]);
 
         var img = document.createElement("img");
-        img.setAttribute("src","img/no-image.png");
+        img.setAttribute("src","img/"+i+"/0.jpg");
+        img.setAttribute("style",
+          "max-width: 100%; " +
+          "height: auto;");
+
+
+        var imgCell = document.createElement("div");
+        imgCell.setAttribute("style",
+          "width: 400px; " +
+          "height: 400px" +
+          "align-items: center;" +
+          "color: white;" +
+          "background-image:  url('img/white.jpg');" +
+          "background-size: 100% 100%;" +
+          "max-width: 400px;" +
+          "background-attachment: fixed;")
+
+        imgCell.appendChild(img);
+
 
         var p = document.createElement("p");
-        p.appendChild(document.createTextNode(data.id[i].price));
+        p.appendChild(document.createTextNode("$"+data.id[i].price));
 
-        var link = document.createElement("A");
+        var link = document.createElement("a");
         link.setAttribute("href","product.html?Id="+i);
         link.setAttribute("name", JSON.stringify(data.id[i].name));
+        link.setAttribute("style","color:white; text-decoration: none;")
         link.appendChild(document.createTextNode(data.id[i].name.toUpperCase()));
-        link.appendChild(img);
+        link.appendChild(imgCell);
+
 
 
         var cell = document.createElement("div");
