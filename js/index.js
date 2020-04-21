0a0f8a5b8008bd833e97b86e4312a41faad7caf0
jeraminl <jeraminl@uci.edu>
2020-04-21 13:21:59 -0700

    add categories to index

--- js/index.js
+++ js/index.js
@@ -18,47 +18,119 @@ function load() {
     })
     .then(function(data) {
       var amount = Object.keys(data.id).length;
-      console.log(amount);
       for (var i = 0; i < amount; i++) {
-        console.log(data.id[i]);
+        if (data.id[i].category == "Office") {
+          var img = document.createElement("img");
+          img.setAttribute("src", "img/" + i + "/0.jpg");
+          img.setAttribute("style", "max-width: 100%; " + "height: auto;");
 
-        var img = document.createElement("img");
-        img.setAttribute("src", "img/" + i + "/0.jpg");
-        img.setAttribute("style", "max-width: 100%; " + "height: auto;");
+          var imgCell = document.createElement("div");
+          imgCell.setAttribute(
+            "style",
+            "width: 400px; " +
+              "height: 400px" +
+              "align-items: center;" +
+              "color: white;" +
+              "background-size: 100% 100%;" +
+              "max-width: 400px;" +
+              "background-attachment: fixed;"
+          );
 
-        var imgCell = document.createElement("div");
-        imgCell.setAttribute(
-          "style",
-          "width: 400px; " +
-            "height: 400px" +
-            "align-items: center;" +
-            "color: white;" +
-            "background-image:  url('img/white.jpg');" +
-            "background-size: 100% 100%;" +
-            "max-width: 400px;" +
-            "background-attachment: fixed;"
-        );
+          imgCell.appendChild(img);
 
-        imgCell.appendChild(img);
+          var p = document.createElement("p");
+          p.appendChild(document.createTextNode("$" + data.id[i].price));
 
-        var p = document.createElement("p");
-        p.appendChild(document.createTextNode("$" + data.id[i].price));
+          var link = document.createElement("a");
+          link.setAttribute("href", "product.html?Id=" + i);
+          link.setAttribute("name", JSON.stringify(data.id[i].name));
+          link.setAttribute("style", "color:white; text-decoration: none;");
+          link.appendChild(
+            document.createTextNode(data.id[i].name.toUpperCase())
+          );
+          link.appendChild(imgCell);
 
-        var link = document.createElement("a");
-        link.setAttribute("href", "product.html?Id=" + i);
-        link.setAttribute("name", JSON.stringify(data.id[i].name));
-        link.setAttribute("style", "color:white; text-decoration: none;");
-        link.appendChild(
-          document.createTextNode(data.id[i].name.toUpperCase())
-        );
-        link.appendChild(imgCell);
+          var cell = document.createElement("div");
+          cell.setAttribute("class", "grid-item");
+          cell.appendChild(link);
+          cell.appendChild(p);
 
-        var cell = document.createElement("div");
-        cell.setAttribute("class", "grid-item");
-        cell.appendChild(link);
-        cell.appendChild(p);
+          document.getElementById("productListOffice").appendChild(cell);
+        } else if (data.id[i].category == "Gaming") {
+          var img = document.createElement("img");
+          img.setAttribute("src", "img/" + i + "/0.jpg");
+          img.setAttribute("style", "max-width: 100%; " + "height: auto;");
 
-        document.getElementById("productList").appendChild(cell);
+          var imgCell = document.createElement("div");
+          imgCell.setAttribute(
+            "style",
+            "width: 400px; " +
+              "height: 400px" +
+              "align-items: center;" +
+              "color: white;" +
+              "background-size: 100% 100%;" +
+              "max-width: 400px;" +
+              "background-attachment: fixed;"
+          );
+
+          imgCell.appendChild(img);
+
+          var p = document.createElement("p");
+          p.appendChild(document.createTextNode("$" + data.id[i].price));
+
+          var link = document.createElement("a");
+          link.setAttribute("href", "product.html?Id=" + i);
+          link.setAttribute("name", JSON.stringify(data.id[i].name));
+          link.setAttribute("style", "color:white; text-decoration: none;");
+          link.appendChild(
+            document.createTextNode(data.id[i].name.toUpperCase())
+          );
+          link.appendChild(imgCell);
+
+          var cell = document.createElement("div");
+          cell.setAttribute("class", "grid-item");
+          cell.appendChild(link);
+          cell.appendChild(p);
+
+          document.getElementById("productListGaming").appendChild(cell);
+        } else if (data.id[i].category == "Professional") {
+          var img = document.createElement("img");
+          img.setAttribute("src", "img/" + i + "/0.jpg");
+          img.setAttribute("style", "max-width: 100%; " + "height: auto;");
+
+          var imgCell = document.createElement("div");
+          imgCell.setAttribute(
+            "style",
+            "width: 400px; " +
+              "height: 400px" +
+              "align-items: center;" +
+              "color: white;" +
+              "background-size: 100% 100%;" +
+              "max-width: 400px;" +
+              "background-attachment: fixed;"
+          );
+
+          imgCell.appendChild(img);
+
+          var p = document.createElement("p");
+          p.appendChild(document.createTextNode("$" + data.id[i].price));
+
+          var link = document.createElement("a");
+          link.setAttribute("href", "product.html?Id=" + i);
+          link.setAttribute("name", JSON.stringify(data.id[i].name));
+          link.setAttribute("style", "color:white; text-decoration: none;");
+          link.appendChild(
+            document.createTextNode(data.id[i].name.toUpperCase())
+          );
+          link.appendChild(imgCell);
+
+          var cell = document.createElement("div");
+          cell.setAttribute("class", "grid-item");
+          cell.appendChild(link);
+          cell.appendChild(p);
+
+          document.getElementById("productListProfessional").appendChild(cell);
+        }
       }
     });
 }
