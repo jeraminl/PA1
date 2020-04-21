c155f989cc061ae4c6d0e6f1ef6a84adaaa4deb2
jeraminl <jeraminl@uci.edu>
2020-04-21 12:51:43 -0700

    prettier formatting

--- js/product.js
+++ js/product.js
@@ -1,73 +1,63 @@
-
-
-
-function load(){
+function load() {
   var params = new URLSearchParams(window.location.search);
-  var productID = params.get('Id');
-
+  var productID = params.get("Id");
 
   var requestInit = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
-    mode: 'cors',
-    cache: 'default' };
+    mode: "cors",
+    cache: "default"
+  };
   let re = new Request("../json/products.json", requestInit);
   fetch(re)
-    .then(function(resp){
+    .then(function(resp) {
       return resp.json();
     })
     .then(function(data) {
       console.log(data.id[productID]);
       document.getElementById("desc").innerHTML =
-        "Description: "+ JSON.stringify(data.id[productID].description);
+        "Description: " + JSON.stringify(data.id[productID].description);
 
-      document.getElementById("productID").innerHTML ="Product ID: " + productID;
-      document.getElementById("price").innerHTML = "Price: " + data.id[productID].price;
-      document.getElementById("size").innerHTML = "Size: " + data.id[productID].size;
-      document.getElementById("key").innerHTML = "Switch: " + data.id[productID].switch;
+      document.getElementById("productID").innerHTML =
+        "Product ID: " + productID;
+      document.getElementById("price").innerHTML =
+        "Price: " + data.id[productID].price;
+      document.getElementById("size").innerHTML =
+        "Size: " + data.id[productID].size;
+      document.getElementById("key").innerHTML =
+        "Switch: " + data.id[productID].switch;
 
-      for (var i = 0; i < 3; i++){
+      for (var i = 0; i < 3; i++) {
         var img0 = document.createElement("img");
         img0.setAttribute("src", data.id[productID].imgPaths[i]);
-        img0.className= ("photo");
-        img0.setAttribute("id", "img"+i);
-        img0.onload = modalZoom(img0)
+        img0.className = "photo";
+        img0.setAttribute("id", "img" + i);
+        img0.onload = modalZoom(img0);
         console.log(img0.id);
 
         var cell = document.createElement("div");
-        cell.className=("image-column");
+        cell.className = "image-column";
         cell.appendChild(img0);
 
         var photos = document.getElementById("image-row");
         photos.appendChild(cell);
       }
-
-
     });
 }
 
-
-function modalZoom(el){
+function modalZoom(el) {
   var modal = document.getElementById("imgModal");
 
   var modalImg = document.getElementById("img");
-  el.onclick = function(){
+  el.onclick = function() {
     modal.style.display = "block";
     modalImg.src = el.src;
-
-  }
+  };
 
   var span = document.getElementsByClassName("close")[0];
   span.onclick = function() {
     modal.style.display = "none";
-  }
+  };
 }
-
-
-
-
-
-
-
--- js/validation.js
+++ js/validation.js
@@ -1,4 +1,4 @@
-function sameAddress(){
+function sameAddress() {
   var yourAddress = document.getElementById("address").value;
   var yourCity = document.getElementById("city").value;
   var yourState = document.getElementById("state").value;
@@ -10,12 +10,6 @@ function sameAddress(){
   document.getElementById("billingZip").value = yourZip;
 }
 
-
-
-
-
-
-
 function validate() {
   if (window.confirm('Click "OK" to confirm your order!')) {
     var yourFirstName = document.getElementById("firstName").value;
@@ -26,7 +20,7 @@ function validate() {
     var yourState = document.getElementById("state").value;
     var yourZip = document.getElementById("zip").value;
 
-    var billFirstName =  document.getElementById("billingFirstName").value;
+    var billFirstName = document.getElementById("billingFirstName").value;
     var billLastName = document.getElementById("billingLastName").value;
     var billAddress = document.getElementById("billingAddress").value;
     var billCity = document.getElementById("billingCity").value;
@@ -34,36 +28,43 @@ function validate() {
     var billZip = document.getElementById("billingZip").value;
     var amount = document.getElementById("units").value;
 
-
-
     var orderNumber = Math.floor(Math.random() * 100000) + 1;
 
-
-
-
-
-
-    window.open("mailto:" +
-
-      encodeURIComponent(yourEmail) +
-      "?subject=Order%20Confirmation%20#" +
-      encodeURIComponent(orderNumber) +
-      "&body=Order%20Details" +
-      "%0d%0aName: " + encodeURIComponent(yourFirstName) + "%20" + encodeURIComponent(yourLastName) +
-      "%0d%0aEmail Address: " + encodeURIComponent(yourEmail) +
-      "%0d%0aAddress: " + encodeURIComponent(yourAddress) +
-      "%0d%0aCity: " + encodeURIComponent(yourCity) +
-      "%0d%0aState: " + encodeURIComponent(yourState) +
-      "%0d%0aZip: " + encodeURIComponent(yourZip)+
-      "%0d%0aBilling" +
-      "%0d%0aName: " + encodeURIComponent(billFirstName) + "%20" + encodeURIComponent(billLastName) +
-      "%0d%0aAddress: " + encodeURIComponent(billAddress) +
-      "%0d%0aCity: " + encodeURIComponent(billCity) +
-      "%0d%0aState: " + encodeURIComponent(billState) +
-      "%0d%0aZip: " + encodeURIComponent(billZip),
-      '_blank',
-      'location=yes,height=570,width=520'
+    window.open(
+      "mailto:" +
+        encodeURIComponent(yourEmail) +
+        "?subject=Order%20Confirmation%20#" +
+        encodeURIComponent(orderNumber) +
+        "&body=Order%20Details" +
+        "%0d%0aName: " +
+        encodeURIComponent(yourFirstName) +
+        "%20" +
+        encodeURIComponent(yourLastName) +
+        "%0d%0aEmail Address: " +
+        encodeURIComponent(yourEmail) +
+        "%0d%0aAddress: " +
+        encodeURIComponent(yourAddress) +
+        "%0d%0aCity: " +
+        encodeURIComponent(yourCity) +
+        "%0d%0aState: " +
+        encodeURIComponent(yourState) +
+        "%0d%0aZip: " +
+        encodeURIComponent(yourZip) +
+        "%0d%0aBilling" +
+        "%0d%0aName: " +
+        encodeURIComponent(billFirstName) +
+        "%20" +
+        encodeURIComponent(billLastName) +
+        "%0d%0aAddress: " +
+        encodeURIComponent(billAddress) +
+        "%0d%0aCity: " +
+        encodeURIComponent(billCity) +
+        "%0d%0aState: " +
+        encodeURIComponent(billState) +
+        "%0d%0aZip: " +
+        encodeURIComponent(billZip),
+      "_blank",
+      "location=yes,height=570,width=520"
     );
-
   }
 }
