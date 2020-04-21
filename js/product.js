682d2f8cc3ce6e6b46ba9cef7edd8163df861ffc
David B. Rodriguez <davidbr@uci.edu>
2020-04-21 15:05:59 -0700

    Dynamic Price
    Made the order form dynamic and allows for users to select various shipping methods/prices and number of units they want to purchase

--- js/product.js
+++ js/product.js
@@ -1,15 +1,14 @@
 function load() {
   var params = new URLSearchParams(window.location.search);
-  var productID = params.get('Id');
-
+  var productID = params.get("Id");
 
   var requestInit = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    mode: 'cors',
-    cache: 'default'
+    mode: "cors",
+    cache: "default",
   };
   let re = new Request("../json/products.json", requestInit);
   fetch(re)
@@ -18,35 +17,50 @@ function load() {
     })
     .then(function (data) {
       console.log(data.id[productID]);
-      document.getElementById("productName").innerHTML = JSON.stringify(data.id[productID].name);
+      document.getElementById("productName").innerHTML = JSON.stringify(
+        data.id[productID].name
+      );
       document.getElementById("desc").innerHTML =
         "Description: " + JSON.stringify(data.id[productID].description);
+      document.getElementById("productID").innerHTML =
+        "Product ID: " + productID;
+      document.getElementById("price").innerHTML =
+        "Price: $" + data.id[productID].price;
+      document.getElementById("size").innerHTML =
+        "Size: " + data.id[productID].size;
+      document.getElementById("key").innerHTML =
+        "Switch: " + data.id[productID].switch;
+      document.getElementById("orderProdID").innerHTML = productID;
+      document.getElementById("orderPrice").innerHTML =
+        data.id[productID].price;
 
-      document.getElementById("productID").innerHTML = "Product ID: " + productID;
-      document.getElementById("price").innerHTML = "Price: $" + data.id[productID].price;
-      document.getElementById("size").innerHTML = "Size: " + data.id[productID].size;
-      document.getElementById("key").innerHTML = "Switch: " + data.id[productID].switch;
+      var ship = document.getElementById("shipMeth");
+      var shipPrice = ship.options[ship.selectedIndex].value;
+      if (shipPrice == 1) {
+        document.getElementById("shipPrice").innerHTML = 10;
+      }
+
+      document.getElementById("orderTotPrice").innerHTML =
+        data.id[productID].price +
+        parseInt(document.getElementById("shipPrice").innerHTML);
 
       for (var i = 0; i < 3; i++) {
         var img0 = document.createElement("img");
         img0.setAttribute("src", data.id[productID].imgPaths[i]);
-        img0.className = ("photo");
+        img0.className = "photo";
         img0.setAttribute("id", "img" + i);
-        img0.onload = modalZoom(img0)
+        img0.onload = modalZoom(img0);
         console.log(img0.id);
 
         var cell = document.createElement("div");
-        cell.className = ("image-column");
+        cell.className = "image-column";
         cell.appendChild(img0);
 
         var photos = document.getElementById("image-row");
         photos.appendChild(cell);
       }
-
-
     });
 }
-
 
 function modalZoom(el) {
   var modal = document.getElementById("imgModal");
@@ -55,11 +69,10 @@ function modalZoom(el) {
   el.onclick = function () {
     modal.style.display = "block";
     modalImg.src = el.src;
-
-  }
+  };
 
   var span = document.getElementsByClassName("close")[0];
   span.onclick = function () {
     modal.style.display = "none";
-  }
+  };
 }
