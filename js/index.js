30beb6c0d38c9ec3791dc965d72ca10dce12d93f
jeraminl <jeraminl@uci.edu>
2020-04-28 15:06:49 -0700

    update index page to use PHP and MySQL

--- js/index.js
+++ js/index.js
@@ -107,3 +107,37 @@ function load() {
       }
     });
 }
+
+
+function fillInfo(){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function (){
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('productListOffice').innerHTML = this.responseText;
+    }
+  };
+  xmlhttp.open("GET", "./php/get_product_office.php",true);
+  console.log("sending");
+  xmlhttp.send();
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function (){
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('productListGaming').innerHTML = this.responseText;
+    }
+  };
+  xmlhttp.open("GET", "./php/get_product_Gaming.php",true);
+  console.log("sending");
+  xmlhttp.send();
+
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function (){
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('productListProfessional').innerHTML = this.responseText;
+    }
+  };
+  xmlhttp.open("GET", "./php/get_product_Professional.php",true);
+  console.log("sending");
+  xmlhttp.send();
+}
