feed89f6b5b1e704b653ba1e4b98c81ab6def84f
jeraminl <jeraminl@uci.edu>
2020-04-28 12:44:07 -0700

    sql script for product finished

---
+++ sql/insert.sql
@@ -0,0 +1,151 @@
+USE store_db;
+
+INSERT INTO products VALUES(
+  0,
+  'Dell Wired Keyboard KB216',
+  'Basic Dell keyboard for office or personal settings',
+  'Full',
+  23,
+  'Membrane',
+  'Office'
+);
+
+INSERT INTO products VALUES(
+  1,
+  'Dell Wireless Keyboard and Mouse KM636',
+  'Contemporary wireless keyboard and mouse for office and personal environments.',
+  'Full',
+  33.94,
+  'Membrane',
+  'Office'
+);
+
+INSERT INTO products VALUES(
+  2,
+  'Redragon K502 RGB Membrane Gaming Keyboard',
+  'An RGB keyboard with membrane switches for all casual gamers.',
+  'Full',
+  26.99,
+  'Membrane',
+  'Gaming'
+);
+
+INSERT INTO products VALUES(
+  3,
+  'Razer Cysnosa RGB Gaming Keyboard',
+  '#1 Best-Selling membrane RGB keyboard for all gamers.',
+  'Full',
+  44.99,
+  'Membrane',
+  'Gaming'
+);
+
+INSERT INTO products VALUES(
+  4,
+  'Redragon K551 Mechanical Gaming Keyboard',
+  'Red back-lit mechanical keyboard with Cherry MX Blue equivalent switches.',
+  'Full',
+  43.99,
+  'Cherry MX Blue Equivalent',
+  'Gaming'
+);
+
+INSERT INTO products VALUES(
+  5,
+  'Redragon S101 Wired Gaming Keyboard and Mouse',
+  'Red back-lit 60% mechanical keyboard with Cherry MX Blue equivalent switches.',
+  'TKL: Tenkeyless',
+  36.99,
+  '',
+  'Gaming'
+);
+
+INSERT INTO products VALUES(
+  6,
+  'Dell Wired Keyboard KB216',
+  'An RGB gaming keyboard and mouse combo for all gamers.',
+  'Full',
+  39.98,
+  'Membrane',
+  'Gaming'
+);
+
+INSERT INTO products VALUES(
+  7,
+  'HyperX Alloy Pro-Tenkeyless Mechanical Keyboard',
+  'Minimalistic TKL gaming keyboard with a steel frame.',
+  'TKL: Tenkeyless',
+  23,
+  'Cherry MX Blue',
+  'Professional'
+);
+
+INSERT INTO products VALUES(
+  8,
+  'Cooler Master MK730 TKL Mechanical Keyboard',
+  'Metal framed TKL Cherry MX Brown keyboard and wrist rest.',
+  'TKL: Tenkeyless',
+  111.99,
+  'Cherry MX Brown',
+  'Professional'
+);
+
+INSERT INTO products VALUES(
+  9,
+  'Corsair K70 RGB Mechanical Gaming Keyboard',
+  'A full mechanical gaming keyboard for gaming and productivity.',
+  'Full',
+  119.99,
+  'Cherry MX Speed',
+  'Gaming'
+);
+
+INSERT INTO products VALUES(
+  10,
+  'DAS Keyboard 4 Professional',
+  'Cherry MX Blue professional mechanical keyboard for Windows and Mac.',
+  'Full',
+  169,
+  'Cherry MX Blue',
+  'Professional'
+);
+
+INSERT INTO products VALUES(
+  11,
+  'HELLO GANSS Full Mechanical Keyboard',
+  'A keyboard best fit for gamers and professionals alike.',
+  'Full',
+  99.99,
+  'Cherry MX Brown',
+  'Gaming'
+);
+
+INSERT INTO products VALUES(
+  12,
+  'HyperX Alloy Elite Mechanical Keyboard',
+  'Customize your RGB keyboard with this mechnical keyboard for elite gamers.',
+  'full',
+  99.99,
+  'Membrane',
+  'Office'
+);
+
+INSERT INTO products VALUES(
+  13,
+  'HELLO GANSS Mini Mechanical Keyboard',
+  'A 60% mechanical keyboard for all professionals.',
+  '',
+  69.99,
+  'Cherry MX Blue',
+  'Professional'
+);
+
+INSERT INTO products VALUES(
+  14,
+  'Logitech G610 Orion Backlit Mechanical Keyboard',
+  'A white back-lit LED keyboard.',
+  'Full',
+  82.45,
+  'Cherry MX Red',
+  'Professional'
+);
